3ea1e0afb2dbd532b0884a08068e9a050b4ec945
khody <ekhodor8@gmail.com>
2024-08-03 15:46:25 -0700

    added admin panel, models are nearly finished.

--- main/admin.py
+++ main/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from . models import Poll, Pollster
 # Register your models here.
 
+from main.models import Pollster
+
 class PollsterAdmin(admin.ModelAdmin):
     list_display = ("name_of_pollster",)
+    
+
+class PollAdmin(admin.ModelAdmin):
+    
+    list_display = ("date_published",)
+
+    
 
 
 admin.site.register(Pollster,PollsterAdmin)
-admin.site.register(Poll)
+admin.site.register(Poll, PollAdmin)
--- main/models.py
+++ main/models.py
@@ -7,18 +7,22 @@ class Pollster(models.Model):
     name_of_pollster = models.CharField(max_length=75)
     info = models.TextField()
     quality_of_pollster = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    def __str__(self):
+        return f"{self.name_of_pollster}"
 
 
 class Poll(models.Model):
     harris_support = models.FloatField(null=False)
     trump_support = models.FloatField(null=False)
     third_party_support = models.FloatField(null=True)
-    
+
     date_published = models.DateField()
     link_to_poll = models.URLField(null=True)
     type_of_voters = models.CharField(max_length=50)
     number_of_respondents = models.IntegerField()
-
-
     pollster = models.ForeignKey(Pollster, on_delete=models.CASCADE,related_name="polls") #if pollster is deleted, models.CASCADE makes it so all polls by said pollster are also deleted.
+    
+    def __str__(self):
+        return f"{self.pollster.name_of_pollster} {self.date_published}"
+    
     
