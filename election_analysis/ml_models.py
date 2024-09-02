c8a597f6c2ba91e70220c99e7b8d5568c67e916c
khody <ekhodor8@gmail.com>
2024-09-01 17:00:10 -0700

    added midterm data going back to 2002, seems like model is seeing some improvement due to this. accuracy: 83.334, also manual seed = 42

--- election_analysis/ml_models.py
+++ election_analysis/ml_models.py
@@ -1,4 +1,5 @@
 import torch
+import random
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
@@ -11,7 +12,8 @@ import sys
 #  
 # dem candidate  support, rep candidate support, dem incumbency, rep incumbency, economic indicator from -1 to 1. -1 = bad, 1 = good
 
-
+torch.manual_seed(42)
+# for midterm elections, incumbency DISadvantage will be applied. i will basically just give however is out of power the 1. 
 # ive made the very debatable assumption that the economic indicator is basically irrelevant if there is no incumbent. 
 X_data = torch.tensor([
     [0.514, 0.442, 1, 0, 0.7], # FDR vs Landon 1936
@@ -31,11 +33,17 @@ X_data = torch.tensor([
     [0.465, 0.354, 0, 1, -0.22], # Clinton v HW bush 1992 
     [0.5219, 0.36125, 1, 0, 0.78], # clinton vs dole 96'
     [0.4382, 0.4694, 0, 0, 0], # gore vs Bush 00'
+    [45, 49.6, 1, 0, 0.06], # midterms 02' house generic ballot - RCP avg incumbency advantages favors democrats as bush in office
     [0.4657, 0.5043, 0, 1, 0.85], # kerry vs bush 04'
+    [40.6, 52.1, 1, 0, -0.16], # midterms 06' generic ballot RCP - avg. incumbency advantage favors democrats as bush in office
     [0.4988, 0.4338, 0, 0, 0], # obama vs mccain 08'
-    [0.48, 0.4656, 1, 0, -0.12], # obama vs romney 2012
+    [41.3, 50.7, 0, 1, -0.03], # midterms 10' generic ballot RCP - avg # incumbency advantage favors republicans as obama in office
+    [0.48, 0.4656, 1, 0, 0.12], # obama vs romney 2012
+    [43.2, 45.6, 0, 1, 0.65], # 2014 midterms generic ballot RCP - avg # incumbency advantges favors reps as obama in office
     [0.4733, 0.4332, 0, 0, 0], # clinton v trump
-    [0.5137, 0.435, 0, 1, -0.2] # biden v trump. while economy had a recession.. it was a quick one due to mass stimulus. so only ~ -0.2
+    [49.7, 42.4, 1, 0, 0.62], # 2018 midterms generic ballot rcp - avg # incumbency favors dems as trump in office
+    [0.5137, 0.435, 0, 1, 0], # biden v trump. while economy had a recession.. it was a quick one due to mass stimulus. so only ~ -0.2
+    [45.5, 48, 0, 1, -0.78] # midterms 2022 
     ], dtype=torch.float32)
 
 #if dem candidates win, y =1, if rep wins y = 0
@@ -58,10 +66,16 @@ y_data = torch.tensor([
     [1],
     [0],
     [0],
+    [0],
     [1],
     [1],
     [0],
-    [1]
+    [1],
+    [0],
+    [0],
+    [1],
+    [1],
+    [0]
 ]).squeeze()
 
 
@@ -106,46 +120,66 @@ print(model_0)
 loss_fn = nn.BCELoss()
 optimizer = optim.Adam(model_0.parameters(), lr = 0.001)
 
-epochs = 10000
+epochs = 1000
 
 def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct/len(y_pred)) * 100
     return acc
+train_model = True
 
-for epoch in range(epochs):
+if train_model:
+    for epoch in range(epochs):
     #1. Forward pass
-    y_probs = model_0(X_training_split).squeeze() # our y probability values, probability of Candidate A winning.
-    candidate_pred = torch.round(y_probs).squeeze()
+        y_probs = model_0(X_training_split).squeeze() # our y probability values, probability of Candidate A winning.
+        candidate_pred = torch.round(y_probs).squeeze()
 
     #2. Determine the loss 
     # loss is determined by seeing if the candidate with the higher probability of winning matches the result.
-    loss = loss_fn(y_probs, y_training_split)
+        loss = loss_fn(y_probs, y_training_split)
     # accuracy function is seeing the training data versus the predicted values and seeing how often it gets it right
-    acc = accuracy_fn(y_true=y_training_split,
+        acc = accuracy_fn(y_true=y_training_split,
                       y_pred=candidate_pred)
     
     #3. zero grad
-    optimizer.zero_grad()
+        optimizer.zero_grad()
 
     #4. Loss backward
-    loss.backward()
+        loss.backward()
 
     #5. 
-    optimizer.step()
+        optimizer.step()
 
-    model_0.eval()
-    with torch.inference_mode():
-        test_y_probs = model_0(X_testing_split).squeeze()
-        test_candidate_pred = torch.round(test_y_probs).squeeze()
+        model_0.eval()
+        with torch.inference_mode():
+            test_y_probs = model_0(X_testing_split).squeeze()
+            test_candidate_pred = torch.round(test_y_probs).squeeze()
 
-        test_loss = loss_fn(test_y_probs, y_testing_split)
+            test_loss = loss_fn(test_y_probs, y_testing_split)
 
-        test_acc = accuracy_fn(y_true=y_testing_split, y_pred=test_candidate_pred)
+            test_acc = accuracy_fn(y_true=y_testing_split, y_pred=test_candidate_pred)
 
-    if epoch % 1000 == 0:
-        print(f"Epoch: {epoch} | Loss: {loss}, Acc: {acc:.2f} | Test loss: {test_loss} Test accuracy: {test_acc}")
-        print(model_0.state_dict())
+        if epoch % 100 == 0:
+            print(f"Epoch: {epoch} | Loss: {loss}, Acc: {acc:.2f} | Test loss: {test_loss} Test accuracy: {test_acc}")
+            print(model_0.state_dict())
+
+print(test_y_probs)
+print(y_data[training_split:])
+
+
+# harris_v_trump = torch.tensor([
+#     [47.1, 43.8, 0, 0, 0]
+# ])
+
+# model_0.eval()
+
+# with torch.inference_mode():
+#     new_data_prediction_probs = model_0(harris_v_trump).squeeze()
+#     new_data_prediction = torch.round(new_data_prediction_probs)
+
+# print(f"Prediction probability: {new_data_prediction_probs}")
+# print(f"Prediction: {new_data_prediction}")
+
 
 
 
